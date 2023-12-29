75c4f16446b7a1e7f5f117f881a6c07d900e7d0f
kyungjae-Y <yoonkj2008@gmail.com>
2023-12-29 17:31:07 +0900

    2023.12.29 17:30
    프로젝트 - 메이플 완성

---
+++ Project_KyungJae/src/mapleGame/GamePoint.java
@@ -0,0 +1,19 @@
+package mapleGame;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class GamePoint extends JLabel {
+	GamePoint gamePoint = this;
+
+	public GamePoint() {
+		setText("Point : 0");
+		setFont(new Font("Point : ", Font.BOLD, 30));
+		setForeground(Color.WHITE);
+		setSize(200, 30);
+		setLocation(1000, 30);
+	}
+}
